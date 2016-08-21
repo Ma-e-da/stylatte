13a58ff28f0399b4e36a3c7e81ceca1717f6a40d
yoshie maeda <yoshie@maeda-no-MacBook-Air.local>
2016-08-22 00:06:11 +0900

    <11.3.3~5>フィードをつくる＆postを削除＆postのテストをつくる

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -19,4 +19,14 @@ class PostsControllerTest < ActionController::TestCase
     end
     assert_redirected_to login_url
   end
+
+#自分以外のユーザーのマイクロポストは削除をしようとすると、適切にリダイレクトされることをテスト
+  test "should redirect destroy for wrong post" do
+    log_in_as(users(:michael))
+    post = posts(:ants)
+    assert_no_difference 'Post.count' do
+      delete :destroy, id: post
+    end
+    assert_redirected_to root_url
+  end
 end
