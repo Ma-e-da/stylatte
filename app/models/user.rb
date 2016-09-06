cfcd90b5b3efaa81b5e64e684eec971677d6a1de
yoshie maeda <yoshie@maeda-no-MacBook-Air.local>
2016-09-06 16:55:04 +0900

    Usersテーブルにpictureカラムを作ったときに、ファイルアップロードできるようにとつけたコード残ってたので今頃とった。

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,6 @@ class User < ActiveRecord::Base
 
   before_save   :downcase_email
   before_create :create_activation_digest
-  mount_uploader :picture, PictureUploader
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
