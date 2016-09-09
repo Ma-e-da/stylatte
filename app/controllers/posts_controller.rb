d74e92ad437396dfd8e34da37fc75ca05ba8ca5c
yoshie maeda <yoshie@maeda-no-MacBook-Air.local>
2016-09-09 21:54:27 +0900

    postsのindexページあたりきれいにみえるように整えた。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,8 +7,6 @@ def new
 end
 
 def index
-  # @posts = current_user.post.paginate(page: params[:page])
-  # @post = current_user.posts
   @posts = current_user.posts.paginate(:page => params[:page], :per_page => 12)
     # @posts = Post.paginate(:page => params[:page], :per_page => 10)
 end
