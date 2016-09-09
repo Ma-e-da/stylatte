6fd22cd83ecae150a4b83040efa4e69649bd0e7c
yoshie maeda <yoshie@maeda-no-MacBook-Air.local>
2016-09-09 22:44:57 +0900

    indexページが動くようにした。（ページネートgemが自動でとってくる_scomment.html.erbファイルもつくった。）

--- app/models/scomment.rb
+++ app/models/scomment.rb
@@ -2,6 +2,7 @@ class Scomment < ActiveRecord::Base
   belongs_to :post
   # スタイリストのユーザーIDをとりたいので
   belongs_to :user
+  
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
 end
