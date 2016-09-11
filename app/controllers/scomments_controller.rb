6f03ac0e3255a3df72e57825221a95e3b2264997
yoshie maeda <yoshie@maeda-no-MacBook-Air.local>
2016-09-11 20:00:24 +0900

    コメント追加。

--- app/controllers/scomments_controller.rb
+++ app/controllers/scomments_controller.rb
@@ -1,12 +1,16 @@
 class ScommentsController < ApplicationController
+
+  # コメントへのリクエストには、その「コメント」に関連している「投稿」の情報が必要になります。
+　#そのため、最初の行でPost.findで「コメント」に関連する「投稿」を取得しています。
   def create
     @post= Post.find(params[:post_id])
     @scomment = @post.scomments.create(scomment_params)
     redirect_to post_path(@post)
   end
+  # アソシエーションを定義したことによって利用可能になったメソッド@post.comments.createは「コメント」を作成し、データベースに保存しています。
 
   private
-  
+
    def scomment_params
      params.require(:scomment).permit(:content)
    end
