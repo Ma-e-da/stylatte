8d7802267685f6238750be7ce0dd27cf3ef5aac8
yoshie maeda <yoshie@maeda-no-MacBook-Air.local>
2016-09-12 09:24:10 +0900

    ポストでupdateができるように、コールバックに付け足した。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :logged_in_user, only: [:new, :index, :show, :create, :destroy]
+  before_action :logged_in_user, only: [:new, :index, :show, :create, :destroy, :update]
   before_action :correct_user,  only: :destroy
 
 def new
@@ -12,6 +12,9 @@ end
 
 def show
   @post = Post.find(params[:id])
+end
+
+def update
 end
 
 def create
