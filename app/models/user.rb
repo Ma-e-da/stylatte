72d0db5405d53b82ac9c0d84b5a171e7a028e76a
yoshie maeda <yoshie@maeda-no-MacBook-Air.local>
2016-09-15 07:14:50 +0900

    モデル作って、アソシエーション関連付けを行った。

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
-
-  has_many :scomments
+  has_many :favorites
+  has_many :scomments, through: :favorites
   has_many :active_relationships, class_name:  "Relationship",
                                   foreign_key: "follower_id",
                                   dependent:  :destroy
@@ -11,10 +11,12 @@ class User < ActiveRecord::Base
                                   dependent:  :destroy
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
+
   attr_accessor :remember_token, :activation_token, :reset_token
 
   before_save   :downcase_email
   before_create :create_activation_digest
+
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -22,7 +24,6 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
 
   has_secure_password
-
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
 
