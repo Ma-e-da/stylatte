2695771fe705005ed227f80d3a848b6962afcbcc
yoshie maeda <yoshie@maeda-no-MacBook-Air.local>
2016-09-24 23:18:54 +0900

    Javascriptファイルも、コメント足した。このディレクトリに作ったファイルはすべて読み込まれるとのことなので、カスタム用のファイル作っておいた。

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,14 +1,12 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
+// これは、マニフェストファイル。一番下のtreeの記述によりこのディレクトリにつくられるすべてのファイルは読み込まれる！
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
-//
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
+
 // 上の２つでjQueryが読み込まれている。
 // turbolinksも読み込んでいる。ページ遷移をAjaxに置き換え、JavaScriptやCSSのパースを省略することで高速化するgem
 //= require jquery
