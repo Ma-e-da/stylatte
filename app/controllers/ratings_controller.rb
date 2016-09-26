05256ea28108994e8f87b5d96451b6e1dd4fe6f5
yoshie maeda <yoshie@maeda-no-MacBook-Air.local>
2016-09-26 23:06:28 +0900

    コントローラー。

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,17 @@
 class RatingsController < ApplicationController
+  def create
+    @user_id = current_user.id
+    @scomments_id = Scomment.find(params[:id]).id
+    @rating = Rating.new(scomment_id: @scomments_id, user_id: @user_id)
+    if @rating.save
+      redirect_to root_url
+    end
+  end
+
+  def destroy
+    @rating = current_user.ratings.find_by!(params[:rating_id])
+    if @rating.destroy
+      redirect_to root_url
+    end
+  end
 end
