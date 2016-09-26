db43bbc5f044884bc24ac36e4d4142e71b0e1f0f
yoshie maeda <yoshie@maeda-no-MacBook-Air.local>
2016-09-26 23:06:03 +0900

    マイグレート。

--- db/migrate/20160926131615_create_ratings.rb
+++ db/migrate/20160926131615_create_ratings.rb
@@ -1,8 +1,11 @@
 class CreateRatings < ActiveRecord::Migration
   def change
     create_table :ratings do |t|
-
+      t.references :user, null: false
+      t.references :scomment, null: false
       t.timestamps null: false
     end
+    add_index :ratings, :user_id
+    add_index :ratings, :scomment_id
   end
 end
