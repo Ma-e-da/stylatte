166576684b83087791bae2ae200d4ee1b3b7e829
yoshie maeda <yoshie@maeda-no-MacBook-Air.local>
2016-09-29 13:01:40 +0900

    ルーティングを変えた。

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,8 @@ Rails.application.routes.draw do
     delete 'logout'  => 'sessions#destroy'
 
 # ユーザーとフォロワー、フォロイング
-# following/followersのIDがいるので、memberにする。（/users/:id/following/:id）
-# 逆に、IDがいらないときは、collectionにする。collection do...end
-# あるユーザーのフォロワー、フォロイングしている人一覧ためget。
+    # following/followersのIDがいる。member（逆は、collection)。(/users/:id/following/:id）
+    # あるユーザーのフォロワー、フォロイングしている人一覧のためgetのみ。
     resources :users do
       member do
         get :following, :followers
@@ -26,14 +25,7 @@ Rails.application.routes.draw do
       resources :scomments, only: [:index, :new, :create]
     end
 
-# お気に入り定義(concern)
-    concern :favoritable do
-      resources :favorites, only: [:new, :create, :index, :destroy]
-    end
-# 星のレーティング定義(concern)
-    concern :ratable do
-      resources :ratings, only: [:create, :update, :index, :destroy]
-    end
+
 
 # アカウントアクティベーションとパスワードリセット
     resources :account_activations, only: [:edit]
@@ -43,18 +35,13 @@ Rails.application.routes.draw do
     resources :relationships,      only: [:create, :destroy]
 
 # IDがいる４つ分。
-    resources :scomments, concerns: [:favoritable, :ratable], only: [:show, :edit, :update, :destroy]
+    resources :scomments, only: [:show, :edit, :update, :destroy]
+
+# お気に入り
+  resources :favorites
+# 星
+  resources :ratings
 
 
-  # 何も、７つのアクションだけでなくても、自分でアクション名をカスタムできる。
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-
-  # コントローラを名前空間によってグループ化、ここではAdmin::名前空間で多数の管理用コントローラ群をまとめている。
-  #   namespace :admin do
-  #     resources :products
-  #   end
-  #     # こう書くことで、Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)とできる。
 
 end
