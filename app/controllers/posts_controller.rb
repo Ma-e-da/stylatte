477205ce13ea6baf256e01c4f9dbcc09b466ebbb
yoshie maeda <yoshie@maeda-no-MacBook-Air.local>
2016-10-01 13:53:53 +0900

    他のユーザーから、自分のポストをみてもらえるようにした。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :logged_in_user, only: [:new, :index, :show, :create, :destroy, :update]
+  before_action :logged_in_user, only: [:new, :index, :create, :destroy, :update]
   before_action :correct_user,  only: :destroy
 
 def new
