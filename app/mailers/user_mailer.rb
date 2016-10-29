3e85708c9878dab23075763e02629f0433a5c2ba
yoshie maeda <yoshie@maeda-no-MacBook-Air.local>
2016-10-29 22:07:46 +0900

    メールデザイン変更。

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,9 @@
 class UserMailer < ApplicationMailer
 
+# ユーザー（変数）と件名をここで指定
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: "Welcome to STYLATTE"
   end
 
   def password_reset(user)
--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,16 +1,22 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+# メールのプレビュー
+
+# プレビューの目次ページ；Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+# 私の場合、Docker使っているので、http://192.168.99.100:3000/rails/mailers/user_mailerでみれる。
+
 class UserMailerPreview < ActionMailer::Preview
 
-  # Preview this email at
+
   # http://localhost:3000/rails/mailers/user_mailer/account_activation
+  # 私の場合、Dockerなので、http://192.168.99.100:3000/rails/mailers/user_mailer/account_activationでみれる。
   def account_activation
     user = User.first
     user.activation_token = User.new_token
     UserMailer.account_activation(user)
   end
 
-  # Preview this email at
+  
   # http://localhost:3000/rails/mailers/user_mailer/password_reset
+  # 私の場合、Docker使っているので、http://192.168.99.100:3000/rails/mailers/user_mailer/password_resetでみれる。
   def password_reset
     user = User.first
     user.reset_token = User.new_token
