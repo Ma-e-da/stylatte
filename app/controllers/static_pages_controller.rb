7187f501156cf7622ab4ffc0dc901bda7188d68c
yoshie maeda <yoshie@maeda-no-MacBook-Air.local>
2016-11-04 14:29:16 +0900

    styliststatusのページの中の表示数がうまく動くようにした。

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -20,7 +20,15 @@ class StaticPagesController < ApplicationController
   def styliststart
   end
 
-  def stylistsanalysis
+  def stylistanalysis
+    @user = current_user
+    @user_id = current_user.id
+    @ratings = Rating.where(scomment_user: @user_id)
+    @total_rating = @ratings.sum(:star)
+    @average_rating = @ratings.average(:star)
+    @scomments = Scomment.where(user_id: @user_id)
+
+
   end
 
   def admin
