62a904cf7c5697d7fb7367573b22ed84d070dff5
yoshie maeda <yoshie@maeda-no-MacBook-Air.local>
2016-11-06 19:52:08 +0900

    usersにpublicカラムを追加した。（デフォルトはtrue=公開）

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104101721) do
+ActiveRecord::Schema.define(version: 20161106103219) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer  "user_id",     null: false
@@ -110,6 +110,7 @@ ActiveRecord::Schema.define(version: 20161104101721) do
     t.string   "hair_color"
     t.string   "eyes_color"
     t.text     "favs"
+    t.boolean  "public",            default: true
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
